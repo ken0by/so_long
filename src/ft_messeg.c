8af7c0a8273d7c15a13bba03d6545e9fada36246
Rodrigo Fuentes <rofuente@c1r2s2.42madrid.com>
2023-06-27 13:15:19 +0200

    OK

--- src/ft_messeg.c
+++ src/ft_messeg.c
@@ -6,7 +6,7 @@
 /*   By: rofuente <rofuente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 16:49:18 by rofuente          #+#    #+#             */
-/*   Updated: 2023/05/12 12:25:57 by rofuente         ###   ########.fr       */
+/*   Updated: 2023/06/27 13:14:34 by rofuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	ft_error(char *s)
 void	ft_win(t_game *game)
 {
 	ft_putstr_fd(GREEN"\n----------------------", 1);
-	ft_putstr_fd("\n¡YOU SAVE PICKLE RICK!\n", 1);
+	ft_putstr_fd("\n¡YOU SAVED PICKLE RICK!\n", 1);
 	ft_putstr_fd("----------------------\n"RESET, 1);
 	ft_close(game);
 	exit(EXIT_SUCCESS);
@@ -33,7 +33,7 @@ void	ft_win(t_game *game)
 void	ft_lose(t_game *game)
 {
 	ft_putstr_fd(RED"\n----------------------", 1);
-	ft_putstr_fd("\n¡YOU KILL PICKLE RICK!\n", 1);
+	ft_putstr_fd("\n¡YOU KILLED PICKLE RICK!\n", 1);
 	ft_putstr_fd("----------------------\n"RESET, 1);
 	ft_close(game);
 	exit(EXIT_SUCCESS);
